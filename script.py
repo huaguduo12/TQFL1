691a67e748449e67eb13a79c2096d6b6fcd8da15
hgd <huaguduo34@gmail.com>
2025-07-31 20:23:21 +0800

    Update script.py

--- script.py
+++ script.py
@@ -18,7 +18,7 @@ COUNTRY_ORDER = [code.strip() for code in COUNTRY_ORDER_STR.split(',')] if COUNT
 
 LINKS_PER_COUNTRY = int(os.getenv("LINKS_PER_COUNTRY") or "20")
 LINK_PREFIX = os.getenv("LINK_PREFIX") or "💖"
-LINK_SUFFIX = os.getenv("LINK_SUFFIX") or "💮"
+LINK_SUFFIX = os.getenv("LINK_SUFFIX") or ""
 
 if not GITHUB_TOKEN or not REPO_NAME or not FILE_PATH: exit(1)
 if not WEBPAGE_URLS: exit(1)
